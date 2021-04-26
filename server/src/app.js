d0b24109f0086aee0288fac73eb496695cb8bb67
zhshakib <zhshakib@gmail.com>
2021-04-27 02:28:48 +0600

    Express app running

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const morgan = require('morgan')
 
 const app = express()
+const port = process.env.PORT || 3000
 
-app.use(morgan('combine'))
 app.use(bodyParser.json())
-app.use(cors)
+app.use(cors())
 
-app.get('/hello', (req, res, next) => {
+app.get('/', (req, res) => {
   res.send({
-    message: 'hello world!'
+    message: 'hello from api'
   })
 })
 
-app.listen(process.env.PORT || 8081)
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
