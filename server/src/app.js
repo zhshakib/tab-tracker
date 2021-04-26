1f047c1f3e2d00bc8d2d0f79e93f93486d320d9f
zhshakib <zhshakib@gmail.com>
2021-04-27 01:31:49 +0600

    Prettier Added and Fixed bug between eslint and prettier

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,18 @@
-console.log('Hello World!')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors)
+
+app.get('/hello', (req, res, next) => {
+  res.send({
+    message: 'hello world!'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
