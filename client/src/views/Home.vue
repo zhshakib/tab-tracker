389afc7b527f7e50f4a89169aadf18d60969aed5
zhshakib <zhshakib@gmail.com>
2021-04-27 12:39:50 +0600

    Router setup Done!

---
+++ client/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h2>Hello</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  
+}
+</script>
+
+
+<style scoped>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
